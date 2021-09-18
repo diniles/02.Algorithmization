1b60b4c5f4bb4a46425a9f64d646347feefe16ae
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-18 16:20:25 +0300

    Add: Arrays - Task 04 finished

--- src/com/dl/arrays/Task04.java
+++ src/com/dl/arrays/Task04.java
@@ -2,5 +2,36 @@
 package com.dl.arrays;
 
 public class Task04 {
+	public static void main(String[] args) {
+		int n = 32;
+		int[] array = new int[n];
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int maxIndex = Integer.MIN_VALUE;
+		int minIndex = Integer.MAX_VALUE;
+		System.out.println("Array before:");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) (Math.random() * n) + 1;
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+				maxIndex = i;
+			}
+			if (array[i] < min) {
+				min = array[i];
+				minIndex = i;
+			}
+		}
+		array[maxIndex] = min;
+		array[minIndex] = max;
+		System.out.println("Array after:");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
 
+	}
 }
