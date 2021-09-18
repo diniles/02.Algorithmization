6acaa8c81ff1c0b53950f64bac8f2a4222900bad
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-18 16:02:35 +0300

    Add: Arrays - Task 03 finished

--- src/com/dl/arrays/Task03.java
+++ src/com/dl/arrays/Task03.java
@@ -3,5 +3,34 @@
 package com.dl.arrays;
 
 public class Task03 {
+	public static void main(String[] args) {
+		int n = 14;
+		int[] array = new int[n];
+		int negative = 0;
+		int positive = 0;
+		int zero = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			if (i % 3 == 0) {
+				array[i] = 0;
+			} else if (i % 5 == 0) {
+				array[i] = i * -1;
+			} else {
+				array[i] = (int) (Math.random() * n) + 1;
+			}
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == 0) {
+				zero++;
+			} else if (array[i] < 0) {
+				negative++;
+			} else {
+				positive++;
+			}
+		}
+		System.out.printf("Negative elements: %d, positive elements: %d, zeroes: %d", negative, positive, zero);
+	}
 
 }
