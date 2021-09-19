602652e30fb83aca15e3438c15b7ab4d623d75d1
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-19 12:12:15 +0300

    Add: Arrays Task 06

---
+++ src/com/dl/arrays/Task06.java
@@ -0,0 +1,9 @@
+//Задана последовательность N вещественных чисел. Вычислить сумму чисел, порядковые номера которых
+//являются простыми числами.
+package com.dl.arrays;
+
+public class Task06 {
+    public static void main(String[] args) {
+
+    }
+}
