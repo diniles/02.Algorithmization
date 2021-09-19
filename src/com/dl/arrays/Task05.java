bd4e90b1f98271c08c3867a12642fd2da5d52007
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-19 12:10:26 +0300

    Add: Arrays Task 05 finished

--- src/com/dl/arrays/Task05.java
+++ src/com/dl/arrays/Task05.java
@@ -2,5 +2,14 @@
 package com.dl.arrays;
 
 public class Task05 {
-
+    public static void main(String[] args) {
+        int number=5;
+        int n=19;
+        int[] array=new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n) + 1;
+            if (array[i]>number)
+            System.out.print(array[i] + " ");
+        }
+    }
 }
