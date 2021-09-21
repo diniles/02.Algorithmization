eb874de6f9cd342704452179cfd9330d06ea06e5
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-21 22:04:01 +0300

    Add: Arrays Task 05 still not finished

--- src/com/dl/arrays/Task06.java
+++ src/com/dl/arrays/Task06.java
@@ -4,6 +4,32 @@ package com.dl.arrays;
 
 public class Task06 {
     public static void main(String[] args) {
-
+        double sumOfPrimeIndex = 0;
+        int n = 5;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.round(Math.random() * n + 1);
+            System.out.print(array[i] + " ");
+        }
+        for (int i = 2; i < array.length; i++) {
+            if (isPrime(i)) {
+                sumOfPrimeIndex += array[i];
+                System.out.println("addded index: "+i);
+            }
+        }
+        System.out.println("\n" + sumOfPrimeIndex);
     }
+
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+
+        int maxIteration = (int) Math.ceil(Math.sqrt(n));
+
+        for (int i = 2; i < maxIteration; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
 }
