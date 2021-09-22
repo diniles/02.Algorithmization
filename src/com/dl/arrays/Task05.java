3cf8ac96eb90a2f41487ba77eb117ae24642c95a
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-22 22:30:07 +0300

    Add: Arrays Task 05 finished

--- src/com/dl/arrays/Task05.java
+++ src/com/dl/arrays/Task05.java
@@ -3,13 +3,21 @@ package com.dl.arrays;
 
 public class Task05 {
     public static void main(String[] args) {
-        int number=5;
-        int n=19;
-        int[] array=new int[n];
+        int n = 19;
+        int[] array = new int[n];
+        System.out.println("Array:");
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * n) + 1;
-            if (array[i]>number)
             System.out.print(array[i] + " ");
+        }
+        
+        System.out.println("\nNow printing only numbers a[i] > i");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > i) {
+                System.out.printf("[%d] - [%d]\n", i, array[i]);
+            } else {
+                System.out.printf("[%d] - skipped\n", i);
+            }
         }
     }
 }
