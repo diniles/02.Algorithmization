d993682bb55596576357570a6250626e3ca8c0d7
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-22 22:01:49 +0300

    Add: Arrays Task 10 finished

--- src/com/dl/arrays/Task10.java
+++ src/com/dl/arrays/Task10.java
@@ -3,4 +3,21 @@
 package com.dl.arrays;
 
 public class Task10 {
+    public static void main(String[] args) {
+        int n = 32;
+        int[] array = new int[n];
+
+        System.out.println("Array before:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n) + 1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\nArray after:");
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 != 0) {
+                array[i] = 0;
+            }
+            System.out.print(array[i] + " ");
+        }
+    }
 }
