52cfd7ddb883b11d4ba8f560d3fd3568c7b9eab4
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-22 22:21:25 +0300

    Add: Arrays Task 09 finished

--- src/com/dl/arrays/Task09.java
+++ src/com/dl/arrays/Task09.java
@@ -3,4 +3,40 @@
 package com.dl.arrays;
 
 public class Task09 {
+    public static void main(String[] args) {
+        int n = 16;
+        int[] array = new int[n];
+
+        System.out.println("Array:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n) + 1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.printf("\nMinimal most common number is: %d", minInArray(array));
+    }
+
+    static int minInArray(int[] array) {
+        int[] popArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    popArray[i]++;
+                }
+            }
+        }
+
+        int valResult = array[0];
+        int popResult = popArray[0];
+        for (int i = 0; i < popArray.length; i++) {
+            if (popArray[i] > popResult) {
+                popResult = popArray[i];
+                valResult = array[i];
+            }
+            if ((popResult == popArray[i]) && (array[i] < valResult)) {
+                valResult = array[i];
+            }
+        }
+        return valResult;
+    }
 }
