5c258bbf07b19c3818bfe6236bdf1d7bf1a46739
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-23 20:22:44 +0300

    Add: Arrays Task 08 finished

--- src/com/dl/arrays/Task08.java
+++ src/com/dl/arrays/Task08.java
@@ -3,5 +3,42 @@
 //исходной те члены, которые равны min(a1,a2,...an)
 package com.dl.arrays;
 
+import java.util.Arrays;
+
 public class Task08 {
+    public static void main(String[] args) {
+        int n = 19;
+        int[] array = new int[n];
+
+        fillArray(array, n);
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+
+        System.out.printf("Minimal number is: %d\n", min);
+
+        int[] arr = returnArray(array, min);
+        System.out.println(arr);
+        System.out.println("Result array is:\n" + Arrays.toString(arr));
+    }
+
+    static void fillArray(int[] array, int n) {
+        System.out.println("Array:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n) + 1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static int[] returnArray(int[] array, int n) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (n != array[i]) result[i] = array[i];
+        }
+        return result;
+    }
+
 }
