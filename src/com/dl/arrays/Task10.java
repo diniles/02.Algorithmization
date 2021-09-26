86e7946aaa00bcfa086cdc98ae58b78be4428367
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-26 14:01:28 +0300

    Fix: format Task10

--- src/com/dl/arrays/Task10.java
+++ src/com/dl/arrays/Task10.java
@@ -2,19 +2,18 @@
 //элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный массив не использовать.
 package com.dl.arrays;
 
-
 public class Task10 {
-    public static void main(String[] args) {
-        int n = 32;
-        
-        int[] array = Service.generateIntArray(n);
-        
-        System.out.println("\nArray after:");
-        for (int i = 0; i < array.length; i++) {
-            if (i % 2 != 0) {
-                array[i] = 0;
-            }
-            System.out.print(array[i] + " ");
-        }
-    }
+	public static void main(String[] args) {
+		int n = 32;
+
+		int[] array = Service.generateIntArray(n);
+
+		System.out.println("\nArray after:");
+		for (int i = 0; i < array.length; i++) {
+			if (i % 2 != 0) {
+				array[i] = 0;
+			}
+			System.out.print(array[i] + " ");
+		}
+	}
 }
