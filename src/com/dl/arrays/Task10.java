bceff126a666ea45a54703a0bd0f6b6a5c51876f
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-26 14:00:20 +0300

    Fix: use Service.generateIntArray() in Task 10

--- src/com/dl/arrays/Task10.java
+++ src/com/dl/arrays/Task10.java
@@ -2,16 +2,13 @@
 //элемент (освободившиеся элементы заполнить нулями). Примечание. Дополнительный массив не использовать.
 package com.dl.arrays;
 
+
 public class Task10 {
     public static void main(String[] args) {
         int n = 32;
-        int[] array = new int[n];
-
-        System.out.println("Array before:");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * n) + 1;
-            System.out.print(array[i] + " ");
-        }
+        
+        int[] array = Service.generateIntArray(n);
+        
         System.out.println("\nArray after:");
         for (int i = 0; i < array.length; i++) {
             if (i % 2 != 0) {
