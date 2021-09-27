183d690dc1f36df427ad8f703f102cc82bf726c4
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-27 20:00:02 +0300

    Add: Arrays of Arrays Tack 01

---
+++ src/com/dl/arraysofarrays/Task01.java
@@ -0,0 +1,12 @@
+package com.dl.arraysofarrays;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m = 5;
+		int n = 5;
+		Service.generateMatrix(m, n);
+	}
+
+}
