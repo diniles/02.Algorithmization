6098fab29e0cd58d4d4b1b01926b769f42f2017d
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-27 20:03:15 +0300

    Fix: method getMatrix fixed

--- src/com/dl/arraysofarrays/Service.java
+++ src/com/dl/arraysofarrays/Service.java
@@ -5,7 +5,7 @@ abstract class Service {
 		int[][] array = new int[m][n];
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[i].length; j++) {
-				array[i][j] = (int) Math.round(Math.random() * m + 1);
+				array[i][j] = (int) Math.round(Math.random() * (m * n) + 1);
 				System.out.print(array[i][j] + " ");
 			}
 			System.out.println();
