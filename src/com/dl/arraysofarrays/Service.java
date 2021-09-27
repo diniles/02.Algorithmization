2518dcec496fb94924e1ea4b45df24c252fb7844
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-27 19:29:28 +0300

    Add: Service class new package Arrays of arrays

---
+++ src/com/dl/arraysofarrays/Service.java
@@ -0,0 +1,14 @@
+package com.dl.arraysofarrays;
+
+abstract class Service {
+	static int[][] generateMatrix(int m, int n) {
+		int[][] array = new int[m][n];
+		for (int i = 0; i < array.length; i++) {
+			
+			
+
+
+		}
+		return array;
+	}
+}array[i]=(int)Math.round(Math.random()*size+1);
