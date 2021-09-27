e4acbf0915b339ca8c450be15acd2b6160599a08
Dzmitry Liashko <dzliashko@gmail.com>
2021-09-27 20:12:11 +0300

    Fix: Arrays of Arrays Tack 01 main function

--- src/com/dl/arraysofarrays/Task01.java
+++ src/com/dl/arraysofarrays/Task01.java
@@ -13,7 +13,7 @@ public class Task01 {
 
 		for (int i = 1; i < matrix.length; i += 2) {
 			if (matrix[i][0] > matrix[i][matrix[i].length - 1]) {
-				for (int j = 0; j < matrix.length; j++) {
+				for (int j = 0; j < matrix[i].length; j++) {
 					System.out.print(matrix[i][j] + " ");
 				}
 			}
